d328d7db82d3eeb492a84e5ffa177d8466b30336
Luis Guilherme Vieira Neves <luisguilhermevnevesnogueira@gmail.com>
2022-04-14 16:32:38 -0300

    Adição da lógica de colocar os paragrafros em li's e colocar na div

--- js/script.js
+++ js/script.js
@@ -147,37 +147,150 @@ function adicionarDiv(){
     spanConteudo.style.fontWeight = '500';
     spanConteudo.style.fontSize = '14px';
     spanConteudo.style.paddingLeft = '22px';
+    // Remover borda do input quando clicado
+    spanConteudo.style.outline = '0px';
 
       // Criando span pra acoplar as imagens B de bold
       let bSpan = document.createElement('span');
       
       // Tag <img> que vai receber a imagem de bold
-      let img = document.createElement('img');
-      img.setAttribute('src','/img/bold.png')
+      let imgBold = document.createElement('img');
+      imgBold.setAttribute('src','/img/bold.png')
 
-      // Adicionando a tag <img> que já tem a imagem bold, dentro da tag span
-      bSpan.appendChild(img)
+      // Adicionando a tag <imgBold> que já tem a imagem bold, dentro da tag span
+      bSpan.appendChild(imgBold)
 
       // # Setei o background de span e alinhei ele pra ficar certinho dentro do discussões container
       bSpan.style.background = '#EAF1F2';
-      bSpan.style.marginTop = '17.4%';
+      bSpan.style.marginTop = '16.8%';
       bSpan.style.position = 'absolute';
       bSpan.style.left = '27.6%';
-
+      bSpan.style.width = '884.9px';
+      bSpan.style.height = '31px';
+      bSpan.style.lineHeight = '36px';
+      
       /*
-        - Alinhei a imagem bold pra ficar no lado esquerdo e coloquei um escutador para que 
-        quando fosse clicado, o texto do input ficasse bold
+      - Alinhei a imagem bold pra ficar no lado esquerdo e coloquei um escutador para que 
+      quando fosse clicado, o texto do input ficasse bold
       */
-      img.style.marginLeft = '-480px';
-      img.style.cursor = 'pointer';
-      img.addEventListener('click', function(){
+     imgBold.style.marginLeft = '-780px';
+     imgBold.style.cursor = 'pointer';
+     imgBold.style.paddingRight = '3%'
+      imgBold.addEventListener('click', function(){
         spanConteudo.style.fontWeight = 'bold';
       })
 
+      
+      
+      
+      // Vou adicionar agora a <img> italico
+      let imgItalico = document.createElement('img');
+      imgItalico.setAttribute('src','/img/italic.png')
+      
+      // Adicionando a tag <imgItalico> que já tem a imagem italico, dentro da tag span
+      bSpan.appendChild(imgItalico);
 
-    // Adicionei a tag <span> que contém <img> dentro do discussões container
+      // Adicionando efeito de cursor no italic
+      imgItalico.style.cursor = 'pointer';
+
+      // # Fazer o mesmo processo de escutar a tag italico e adicionar efeito italico
+      imgItalico.addEventListener('click', function(){
+        spanConteudo.style.fontStyle = 'italic';
+      })
+
+
+    // Adicionei o {input} que o usuário vai digitar dentro do container  
     discussoes_container.appendChild(spanConteudo);
+    
+    // Adicionei a tag <span> que contém <img Bold e Italic> dentro do discussões container
     discussoes_container.appendChild(bSpan);
+
+
+
+    // Criação do botão para enviar o tópico
+    let buttonEnviarTopico = document.createElement('button');
+    buttonEnviarTopico.innerText = 'Enviar';
+    buttonEnviarTopico.style.textAlign = 'center';
+    buttonEnviarTopico.style.background = 'linear-gradient(#FEB154, #F1833E)';
+    buttonEnviarTopico.style.position = 'absolute';
+    buttonEnviarTopico.style.top = '135%';
+    buttonEnviarTopico.style.left = '80.7%';
+    buttonEnviarTopico.style.width = '205px';
+    buttonEnviarTopico.style.height = '31px';
+    buttonEnviarTopico.style.border = 'none';
+    buttonEnviarTopico.style.color = '#FFF';
+    buttonEnviarTopico.style.backgroundHover = '#000';
+    buttonEnviarTopico.style.cursor = 'pointer';
+
+    discussoes_container.appendChild(buttonEnviarTopico);
+  
+
+    // # Ao clicar no botão de enviar, os elementos precisam ser criados
+    buttonEnviarTopico.addEventListener('click', function(e){
+      let contadorClick = e.target.value;
+
+      for(let i=0; i <= contadorClick; i++){
+        // A cada clique do botão enviar, vai ser criado a estrutura
+
+        // # Criação do <ul>
+        let ul = document.createElement('ul');
+
+        // # Adicionando a classe ul na tag <ul> para pegar a estilização
+        ul.classList.add('ul');
+
+        // # Criando <li> pra guardar o titulo do assunto do usuário
+        let li_Assunto = document.createElement('li');
+
+
+        // # Adicionando id #assunto-pergunta 
+        li_Assunto.id = 'assunto-pergunta';
+        
+
+        // # Guardando o valor do input do usuário dentro da minha li
+        li_Assunto.innerText += spanAssunto.innerText; 
+
+
+        // # Criando <li> para guardar o autor da mensagem
+        let li_autor = document.createElement('li');
+        li_autor.id = 'autor';
+        /*
+          Como eu não tenho um campo para o valor do autor do texto, vou criar um autor aleatorio
+          com a biblioteca matematica
+        */
+        li_autor.innerHTML += `UsuarioAnonimo ${Math.floor(Math.random() * 100)}`;
+        
+
+
+        // # Criando <li> para colocar o comecinho da pergunta
+        let li_comecinhoPergunta = document.createElement('li');
+
+        // ID que vai ficar o comecinho da pergunta
+        let comecinho_Pergunta = document.querySelector('#comecinho-Pergunta');
+
+        // Limpando comecinho_Pergunta
+        comecinho_Pergunta.innerText = '';
+
+        // Vou colocar o começo do assunto que o usuário escreve
+        comecinho_Pergunta.innerText += spanConteudo.innerText;
+
+
+        // Agora vou adicionar na tag <li>
+        li_comecinhoPergunta.append(comecinho_Pergunta);
+
+
+        // Montando toda a estrutura 
+        ul.appendChild(li_Assunto);
+        ul.appendChild(li_autor);
+        ul.appendChild(li_comecinhoPergunta);
+
+        console.log(ul);
+       
+      }
+
+    })
+  
+  
+
 
 
   })
@@ -185,12 +298,11 @@ function adicionarDiv(){
 }
 
 /*
-<ul class="ul">
-  <li id="assunto-pergunta">Assunto da pergunta aparece aqui</li>
-  <li id="autor">Carlos Henrique Santos</li>
+
   <li>
     <p>Comecinho da pergunta aparece aqui resente relato inscreve-se no campo da análise da dimensão e impacto de processo formativo situado impacto de processo formativo processo...</p>
   </li>
+
   <li>
     <ul id="likes-respostas">
       <li><img src="./img/list.png" alt="list icon" id="list-icon"></li>
@@ -199,7 +311,7 @@ function adicionarDiv(){
       <li>1 resposta</li>
     </ul>
   </li>
-</ul> 
+
 */
 
 
